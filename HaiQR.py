88b4e7f06169149131b4c3bbf841c2536ce1b764
Fabio Tomat <f.t.public@gmail.com>
2021-08-12 14:47:54 +0000

    About window and cleanups

--- HaiQR.py
+++ HaiQR.py
@@ -133,7 +133,7 @@ class HaiQRWindow(BWindow):
 		#zonte pview
 		self.qrframe=PView((l+15,t+15,r-15,b-70),"photoframe",None)
 		self.underlist.AddChild(self.qrframe)
-		self.imginmemory = False  #boolean that enables "save to disk" function
+		#self.imginmemory = False  #boolean that enables "save to disk" function
 		self.fp=BFilePanel.BFilePanel(B_SAVE_PANEL)
 		self.fp.SetPanelDirectory("/boot/home/Desktop")
 		self.fp.SetSaveText("prova.png")
@@ -178,10 +178,7 @@ class HaiQRWindow(BWindow):
 			savepath= BEntry.BEntry(txt,True).GetPath().Path()
 			e = msg.FindString("name")
 			completepath = savepath +"/"+ e
-			print ("procedo a salvare " + completepath)
 			self.qrimg.save(completepath)
-			#print savepath.Path()
-			
 
 		if msg.what == 3:
 			#ABOUT
@@ -211,12 +208,7 @@ class HaiQRWindow(BWindow):
 						self.ofp.Show()
 						self.CanOpenPanel=False
 			return
-#		if msg.what == B_ENTER:
-#			#Se o frachi Invie o prepari il QR
-#			if self.tachetest != "":
-#				print "Gjenere il QR cun Invie"
-#				BApplication.be_app.WindowAt(0).PostMessage(BMessage(1))
-#			return
+
 		if msg.what == 6:
 			self.CanOpenPanel=True
 			
@@ -243,25 +235,26 @@ class AboutWindow(BWindow):
 
 	def __init__(self):							
 		BWindow.__init__(self, self.kWindowFrame, self.kWindowName, B_MODAL_WINDOW, B_NOT_RESIZABLE|B_WILL_DRAW)
+		bbox=BBox((0,0,500,470), 'underbox', B_FOLLOW_ALL, B_WILL_DRAW|B_NAVIGABLE, B_NO_BORDER)
+		self.AddChild(bbox)
 		self.CloseButton = BButton(self.kButtonFrame, self.kButtonName, self.kButtonName, BMessage(self.BUTTON_MSG))		
-		cise=(10,4,485,420)
-		cjamput=(0,0,475,420)
+		cise=(10,4,490,420)
+		cjamput=(0,0,480,420)
 		self.messagjio= BTextView(cise, 'TxTView', cjamput, B_FOLLOW_ALL, B_WILL_DRAW)
 		self.messagjio.SetStylable(1)
 		self.messagjio.MakeSelectable(0)
 		self.messagjio.MakeEditable(0)
-		stuff = '\n\t\t\t\t\t\t\tBulletin Gator\n\n\t\t\t\t\t\t\t\t\t\t\tA simple feed aggregator\n\t\t\t\t\t\t\t\t\t\t\tfor Haiku, version 0.8.2\n\t\t\t\t\t\t\t\t\t\t\tcodename "Hully Gully"\n\n\t\t\t\t\t\t\t\t\t\t\tby Fabio Tomat aka TmTFx\n\t\t\t\t\t\t\t\t\t\t\te-mail:\n\t\t\t\t\t\t\t\t\t\t\tf.t.public@gmail.com\n\n\t\t\t\t\t\t\t\t\t\t\tspecial thanks to:\n\t\t\t\t\t\t\t\t\t\t\tVanessa W. x translations\n\nGNU GENERAL PUBLIC LICENSE:\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see \n<http://www.gnu.org/licenses/>'
-		n = stuff.find('Bulletin Gator')
+		stuff = '\n\t\t\t\t\t\t\t\tHaiQR\n\n\t\t\t\t\t\t\t\t\t\t\tA simple QR generator\n\t\t\t\t\t\t\t\t\t\t\tfor Haiku, version 0.1\n\t\t\t\t\t\t\t\t\t\t\tcodename "Cure"\n\n\t\t\t\t\t\t\t\t\t\t\tby Fabio Tomat aka TmTFx\n\t\t\t\t\t\t\t\t\t\t\te-mail:\n\t\t\t\t\t\t\t\t\t\t\tf.t.public@gmail.com\n\n\n\n\nGNU GENERAL PUBLIC LICENSE:\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see \n<http://www.gnu.org/licenses/>'
+		n = stuff.find('HaiQR')
 		m = stuff.find('This')
-		self.messagjio.SetText(stuff, [(0, be_plain_font, (0, 0, 0, 0)), (n, be_bold_font, (0, 150, 0, 0)), (n + 14, be_plain_font, (0, 0, 0, 0)),(m,be_plain_font,(100,150,0,0))])
-		self.AddChild(self.messagjio)
-		self.AddChild(self.CloseButton)
+		self.messagjio.SetText(stuff, [(0, be_plain_font, (0, 0, 0, 0)), (n, be_bold_font, (0, 150, 0, 0)), (n + 5, be_plain_font, (0, 0, 0, 0)),(m,be_plain_font,(100,150,0,0))])
+		bbox.AddChild(self.messagjio)
+		bbox.AddChild(self.CloseButton)
 		self.CloseButton.MakeFocus(1)
-		link=sys.path[0]+"/help/bgator5.png"
+		link=sys.path[0]+"/data/HaiQR.png"
 		self.img=BTranslationUtils.GetBitmap(link)
-		#self.img.Bounds()
-		self.photoframe=PView((0,50,315,185),"photoframe",self.img)
-		self.AddChild(self.photoframe)
+		self.photoframe=PView((40,40,255,255),"photoframe",self.img)
+		bbox.AddChild(self.photoframe)
 
 	def MessageReceived(self, msg):
 		if msg.what == self.BUTTON_MSG:
@@ -291,7 +284,7 @@ class HaiQRApplication(BApplication.BApplication):
 					a=BMessage(112)
 					a.AddString("path=",self.txtpath)
 					BApplication.be_app.WindowAt(0).PostMessage(a)
-				except: #BMessage.error, val:
+				except:
 					e = None
 				if e is None:
 					break
@@ -299,7 +292,6 @@ class HaiQRApplication(BApplication.BApplication):
 				
 	def MessageReceived(self, msg):
 		if msg.what == B_SAVE_REQUESTED:
-			#x = self.window.fp.GetPanelDirectory()
 			e = msg.FindString("name")
 			messaggio = BMessage(54173)
 			messaggio.AddString("name",e)
@@ -316,8 +308,7 @@ class HaiQRApplication(BApplication.BApplication):
 				BApplication.be_app.WindowAt(0).PostMessage(BMessage(4))
 				BApplication.be_app.WindowAt(0).PostMessage(BMessage(6))
 			return
-		if msg.what == B_STRING_TYPE:
-			print "waaa"
+			
 		if msg.what == 311:
 			self.txtpath = ""
 			return
